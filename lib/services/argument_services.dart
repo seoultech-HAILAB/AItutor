84f1d4db6db7a8a5be0d2feabf9bf5de35f8b44d
sabin5105 <sabin5105@gmail.com>
2024-08-11 17:58:26 +0900

    fix: db and viewer

--- lib/services/argument_services.dart
+++ lib/services/argument_services.dart
@@ -9,7 +9,7 @@ class ArgumentServices {
   // 노트앱 제출
   Future<List> saveNote({required String key, required String uid, required String contents}) async {
     try {
-      final databaseRef = FirebaseDatabase.instance.ref('Chat/AI 튜터 논증 및 글쓰기 챗봇/note/$key/$uid').push();
+      final databaseRef = FirebaseDatabase.instance.ref('Chat/AI 글쓰기 튜터/note/$key/$uid').push();
       String timestamp = DateTime.now().toIso8601String();  // Timestamp를 데이터베이스에 오류없이 담기위해 String타입으로 변환
       await databaseRef.set({
         'time': timestamp,
@@ -27,7 +27,7 @@ class ArgumentServices {
   // 읽기자료 로드
   Future<List> getDocs() async {
     try {
-      final dataRes = await FirebaseDatabase.instance.ref('Chat/AI 튜터 논증 및 글쓰기 챗봇/docs').get();
+      final dataRes = await FirebaseDatabase.instance.ref('Chat/AI 글쓰기 튜터/docs').get();
 
       Map<String, dynamic> dataMap = !dataRes.exists ? {} : Map<String, dynamic>.from(json.decode(json.encode(dataRes.value)));
       List keyList = dataMap.keys.toList();
