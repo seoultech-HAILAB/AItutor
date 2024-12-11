3d908f3a1c50229b65b6380a3825d410e52948de
Seung Eon Cha <winun1127@gmail.com>
2024-12-11 23:56:16 +0900

    fix: pixels overflowed

--- lib/pages/select_type_page.dart
+++ lib/pages/select_type_page.dart
@@ -23,17 +23,17 @@ class SelectTypePage extends StatefulWidget {
 class _SelectTypePageState extends State<SelectTypePage> {
   ColorsModel _colorsModel = ColorsModel();
   bool _loading = false;
-  List<ChatModel> _chatModels = [];  // 서버에 저장된 타입들을 저장하는 변수
+  List<ChatModel> _chatModels = []; // 서버에 저장된 타입들을 저장하는 변수
   PageProvider _pageProvider = PageProvider();
   UserModel _userModel = UserModel();
-  Map _linkedTimeMap = {};    // 접속기록
+  Map _linkedTimeMap = {}; // 접속기록
   Map _hasEvaluateHistory = {};
 
   @override
   void initState() {
     super.initState();
     userInit();
-    typesInit();  // 서버에서 타입리스트 로드
+    typesInit(); // 서버에서 타입리스트 로드
   }
 
   @override
@@ -44,42 +44,50 @@ class _SelectTypePageState extends State<SelectTypePage> {
     var screenWidth = MediaQuery.of(context).size.width;
 
     // 가로 사이즈에 따라서 플랫폼 구별
-    bool isWeb = ClassificationPlatform().classifyWithScreenSize(context: context) == 2;
+    bool isWeb =
+        ClassificationPlatform().classifyWithScreenSize(context: context) == 2;
 
     return Stack(
       children: [
-        !isWeb ?
-        Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: _chatModels.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(left: 15, right: 15, bottom: 30),
-                      child: typeWidget(_chatModels[index], screenWidth, isWeb),
-                    );
-                  }),
-            ),
-          ],
-        )   // 모바일일 경우에 UI
-            :
-        Padding(
-          padding: const EdgeInsets.only(left: 60, right: 60, bottom: 30),
-          child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-                childAspectRatio: 3 / 1.2, //item 의 가로, 세로의 비율
-                mainAxisSpacing: 30, //수직 Padding
-                crossAxisSpacing: 100, //수평 Padding
-              ),
-              itemCount: _chatModels.length,
-              itemBuilder: (BuildContext context, int index) {
-
-                return typeWidget(_chatModels[index], screenWidth, isWeb);
-              }),
-        ),  // 웹일 경우의 UI
-        _loading ? Center(child: CircularProgressIndicator(color: _colorsModel.main,),) : Container(),
+        !isWeb
+            ? Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: _chatModels.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15, right: 15, bottom: 30),
+                            child: typeWidget(
+                                _chatModels[index], screenWidth, isWeb),
+                          );
+                        }),
+                  ),
+                ],
+              ) // 모바일일 경우에 UI
+            : Padding(
+                padding: const EdgeInsets.only(left: 60, right: 60, bottom: 30),
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
+                      childAspectRatio: 3 / 2, //item 의 가로, 세로의 비율 TODO: 여기야!
+                      mainAxisSpacing: 30, //수직 Padding
+                      crossAxisSpacing: 100, //수평 Padding
+                    ),
+                    itemCount: _chatModels.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return typeWidget(_chatModels[index], screenWidth, isWeb);
+                    }),
+              ), // 웹일 경우의 UI
+        _loading
+            ? Center(
+                child: CircularProgressIndicator(
+                  color: _colorsModel.main,
+                ),
+              )
+            : Container(),
       ],
     );
   }
@@ -100,7 +108,7 @@ class _SelectTypePageState extends State<SelectTypePage> {
         imagePath = "assets/icons/stress.png";
         break;
       default:
-        imagePath = "assets/icons/default.png";  // 기본 이미지 경로 설정
+        imagePath = "assets/icons/default.png"; // 기본 이미지 경로 설정
     }
 
     return Container(
@@ -118,28 +126,36 @@ class _SelectTypePageState extends State<SelectTypePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ClipRRect(
-                  borderRadius: BorderRadius.circular(12),  // 둥근 사각형을 위해 모서리 반경 설정
+                  borderRadius:
+                      BorderRadius.circular(12), // 둥근 사각형을 위해 모서리 반경 설정
                   child: Container(
                     width: 80,
                     height: 80,
                     child: Image.asset(imagePath, fit: BoxFit.cover),
                   ),
                 ),
-                const SizedBox(width: 15,),
+                const SizedBox(
+                  width: 15,
+                ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("${chatModel.key ?? ''}", style: const TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontFamily: 'Cafe24Oneprettynight'
-                    ),),
-                    Text("${_linkedTimeMap[chatModel.key] ?? "${chatModel.key}와 대화해보세요!"}", style: TextStyle(
-                      fontSize: 25,
-                      color: const Color(0xff3d3d3d),
-                      fontFamily: 'Cafe24Oneprettynight'
-                    ),textAlign: TextAlign.left,),
+                    Text(
+                      "${chatModel.key ?? ''}",
+                      style: const TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontFamily: 'Cafe24Oneprettynight'),
+                    ),
+                    Text(
+                      "${_linkedTimeMap[chatModel.key] ?? "${chatModel.key}와 대화해보세요!"}",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: const Color(0xff3d3d3d),
+                          fontFamily: 'Cafe24Oneprettynight'),
+                      textAlign: TextAlign.left,
+                    ),
                   ],
                 ),
                 const Spacer(),
@@ -155,14 +171,25 @@ class _SelectTypePageState extends State<SelectTypePage> {
                 ),
               ],
             ),
-            const SizedBox(height: 10,),
-            Text("${chatModel.explain ?? ''}", style: TextStyle(
-              fontSize: 25,
-              color: const Color(0xff3d3d3d),
-              fontFamily: 'Cafe24Oneprettynight'
-            ),textAlign: TextAlign.left,),
-            !isWeb ? const SizedBox(height: 20,) : const Spacer(),  // 넓힐 수 있는 최대 간격을 넓혀줌
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "${chatModel.explain ?? ''}",
+              style: TextStyle(
+                  fontSize: 25,
+                  color: const Color(0xff3d3d3d),
+                  fontFamily: 'Cafe24Oneprettynight'),
+              textAlign: TextAlign.left,
+            ),
+            !isWeb
+                ? const SizedBox(
+                    height: 20,
+                  )
+                : const Spacer(), // 넓힐 수 있는 최대 간격을 넓혀줌
+            const SizedBox(
+              height: 10,
+            ),
             Padding(
               padding: const EdgeInsets.only(left: 40, right: 40),
               child: GestureDetector(
@@ -175,7 +202,8 @@ class _SelectTypePageState extends State<SelectTypePage> {
                     _pageProvider.updatePage(1);
                   }
                 },
-                child: MouseRegion( // 마우스를 감지하여 마우스 모양을 띄워줌
+                child: MouseRegion(
+                  // 마우스를 감지하여 마우스 모양을 띄워줌
                   cursor: SystemMouseCursors.click,
                   child: Container(
                     decoration: BoxDecoration(
@@ -188,10 +216,9 @@ class _SelectTypePageState extends State<SelectTypePage> {
                         child: Text(
                           _getButtonText(chatModel.type),
                           style: const TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Cafe24Oneprettynight'
-                          ),
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Cafe24Oneprettynight'),
                         ),
                       ),
                     ),
@@ -199,42 +226,49 @@ class _SelectTypePageState extends State<SelectTypePage> {
                 ),
               ),
             ),
-            const SizedBox(height: 10,),
-            _hasEvaluateHistory[chatModel.key] == true ?
-            Padding(
-              padding: const EdgeInsets.only(left: 40, right: 40),
-              child: GestureDetector(
-                onTap: () async {
-                  _pageProvider.updateIsFromChat(false);
-                  _pageProvider.updateChatModel(chatModel);
-                  if (chatModel.type == "debate") {
-                    _pageProvider.updatePage(2);
-                  } else if (chatModel.type == "stress"){
-                    _pageProvider.updatePage(4);
-                  } else if (chatModel.type == "argument") {
-                    _pageProvider.updatePage(6);
-                  } 
-                },
-                child: MouseRegion( // 마우스를 감지하여 마우스 모양을 띄워줌
-                  cursor: SystemMouseCursors.click,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: _colorsModel.wh,
-                      borderRadius: BorderRadius.circular(12),
+            const SizedBox(
+              height: 10,
+            ),
+            _hasEvaluateHistory[chatModel.key] == true
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 40, right: 40),
+                    child: GestureDetector(
+                      onTap: () async {
+                        _pageProvider.updateIsFromChat(false);
+                        _pageProvider.updateChatModel(chatModel);
+                        if (chatModel.type == "debate") {
+                          _pageProvider.updatePage(2);
+                        } else if (chatModel.type == "stress") {
+                          _pageProvider.updatePage(4);
+                        } else if (chatModel.type == "argument") {
+                          _pageProvider.updatePage(6);
+                        }
+                      },
+                      child: MouseRegion(
+                        // 마우스를 감지하여 마우스 모양을 띄워줌
+                        cursor: SystemMouseCursors.click,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: _colorsModel.wh,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.only(top: 10, bottom: 10),
+                            child: Center(
+                              child: Text(
+                                "평가 기록 보기",
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Cafe24Oneprettynight'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                    child: const Padding(
-                      padding: EdgeInsets.only(top: 10, bottom: 10),
-                      child: Center(
-                        child: Text("평가 기록 보기", style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Cafe24Oneprettynight'
-                        ),),
-                      ),),
-                  ),
-                ),
-              ),
-            ) : Container(),
+                  )
+                : Container(),
           ],
         ),
       ),
@@ -259,7 +293,7 @@ class _SelectTypePageState extends State<SelectTypePage> {
       _loading = true;
     });
     print("typesInit called");
-    List typesResList = await ChatServices().getTypeList();  // 서버에서 타입리스트 로드
+    List typesResList = await ChatServices().getTypeList(); // 서버에서 타입리스트 로드
     print("typesResList: $typesResList");
 
     if (typesResList.first) {
@@ -268,7 +302,8 @@ class _SelectTypePageState extends State<SelectTypePage> {
 
       for (int i = 0; i < chatModels.length; i++) {
         String chatModelKey = chatModels[i].key ?? "";
-        bool isHas = await ChatServices().hasEvaluateHistory(uid: AuthService().getUid(), chatModelKey: chatModelKey);
+        bool isHas = await ChatServices().hasEvaluateHistory(
+            uid: AuthService().getUid(), chatModelKey: chatModelKey);
         hasEvaluateHistory[chatModelKey] = isHas;
       }
 
@@ -285,7 +320,8 @@ class _SelectTypePageState extends State<SelectTypePage> {
 
   // 서버에서 유저정보를 가져옴
   Future<void> userInit() async {
-    List resList = await UserServices().getUserModel(uid: AuthService().getUid());
+    List resList =
+        await UserServices().getUserModel(uid: AuthService().getUid());
     if (resList.first) {
       UserModel userModel = resList.last;
       Map linkedTime = userModel.linkedTime ?? {};
@@ -293,7 +329,8 @@ class _SelectTypePageState extends State<SelectTypePage> {
       if (linkedTime.isNotEmpty) {
         List modeList = linkedTime.keys.toList();
         for (int i = 0; i < modeList.length; i++) {
-          linkedTimeMap[modeList[i]] = timeDifference(linkedTime[modeList[i]] ?? "");
+          linkedTimeMap[modeList[i]] =
+              timeDifference(linkedTime[modeList[i]] ?? "");
         }
       }
       setState(() {
